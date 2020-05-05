c9c799aa64300a5d43cfb4fb839de33ba2bbb494
Kanwal Nayyer <kanwal.tbox@gmail.com>
2020-05-05 15:48:51 +0500

    - share button functionality added

--- NewsFeed/app/src/main/java/com/news/newsfeed/ui/NewsDetail/NewsDetail.java
+++ NewsFeed/app/src/main/java/com/news/newsfeed/ui/NewsDetail/NewsDetail.java
@@ -3,8 +3,10 @@ package com.news.newsfeed.ui.NewsDetail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +23,7 @@ public class NewsDetail extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_news_detail,false);
         registerViews();
+        setupShareButton(getString(R.string.dummy_content));
     }
 
     private void registerViews()
@@ -43,5 +46,21 @@ public class NewsDetail extends BaseActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setupShareButton(final String article) {
+        shareNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // default share intent of mobile will open and news content will be shared through chosen item
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                //String shareText = article.getTitle() + "\n" + article.getUrl();
+                String shareText = article;
+                intent.putExtra(Intent.EXTRA_TEXT, shareText);
+                intent.setType("text/plain");
+
+                startActivity(intent);
+            }
+        });
     }
 }
